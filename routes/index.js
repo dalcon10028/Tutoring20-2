2316633a438394bdb2d46a39747ca0c367e61c62
연근 <dalcon10280@gmail.com>
2020-11-12 04:54:26 +0000

    카카오 로그인 1단계

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,19 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+const passport = require('passport');
+const social_login = require('../my_modules/social_login');
+const KakaoStrategy = require('passport-kakao').Strategy;
+
+// 로그인 처리 - 카카오
+passport.use('login-kakao', new KakaoStrategy({
+		clientID : '11648f24758196fb487c9f669f287019',
+		callbackURL : 'https://todolist-yxmum.run.goorm.io/login/kakao/callback' // 카카오 개발자 사이트에서 지정한 리다이렉트 URL 
+	},
+	function(accessToken, refreshToken, profile, done) {
+		console.log(profile);
+		return done(null, profile);
+	}
+));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -9,5 +23,13 @@ router.get('/', function(req, res, next) {
 router.get('/login', function(req, res, next) {
   res.render('login');
 });
+
+router.get('/login/kakao', passport.authenticate('login-kakao'));
+
+router.get('/login/kakao/callback', passport.authenticate('login-kakao', {
+    successRedirect: '/',
+    failureRedirect: '/login'
+}));
+
 
 module.exports = router;
