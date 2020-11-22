036635188e62384e1b7d6f384c75eedc7f29d6d0
dalcon10028 <dalcon10280@gmail.com>
2020-11-22 20:34:25 +0900

    로컬 채팅 구현
    로컬 채팅 구현

--- public/js/script.js
+++ public/js/script.js
@@ -1,1 +1,15 @@
+// 소켓 연결
 const socket = io();
+// 채팅 내용들
+let messages = [];
+
+$('#userinput').on('keypress', (e)=>{
+// 인풋박스 엔터 누르고 공백 입력이 아닐 때
+  if(e.key==='Enter' && $('#userinput').val()!==''){
+    let msg = $('#userinput').val();
+    // 입력창 비우기
+    $('#userinput').val('');
+    // 채팅 입력
+    $('#chat_box').append(`<li class="list-group-item">나 : ${msg}</li>`);
+  }
+})
